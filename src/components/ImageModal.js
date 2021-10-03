afc7895c9f024b91358ea382e8b43bef8b04af72
JesseNeon <jessejoseph64@gmail.com>
2021-10-03 01:13:27 +0100

    Modal completed

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import "./ImageModal.css";
+import ada from "../assets/images/ada.png";
+import { ImCross } from "react-icons/im";
+import { hideModal } from "../store/actions/imageModal";
+import { useDispatch } from "react-redux";
 
 export default function ImageModal({ imageSource }) {
   const modalState = useSelector((store) => store.imageModal);
+  const dispatch = useDispatch();
+  const modalRef = useRef();
+
+  const handleClick = (e) => {
+    if (e.target === modalRef.current) {
+      dispatch(hideModal());
+    }
+  };
   return (
     <div
       style={{
@@ -11,7 +23,19 @@ export default function ImageModal({ imageSource }) {
       }}
       className="imageModal"
     >
-      Modal
+      <div
+        ref={modalRef}
+        onClick={(e) => handleClick(e)}
+        className="imageModal__wrapper"
+      >
+        <div className="imageModal__cancel">
+          <ImCross
+            onClick={() => dispatch(hideModal())}
+            className="imageModal__icon"
+          />
+        </div>
+        <img src={ada} className="imageModal__image" />
+      </div>
     </div>
   );
 }
