e9099ba36de17ba09010e8e4dfcb50bbb35d8c71
Ini10000 <iniobong10000@gmail.com>
2021-10-06 23:51:58 +0100

    Few changes to file structure and implementations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { RiSearchLine } from "react-icons/ri";
-import "./Header.css";
+import { NavLink } from "react-router-dom";
+
+import "../assets/css/Header.css";
 
 export default function Header() {
   return (
     <div className="header">
       <div className="header__wrapper">
         <div className="header__logo">
-          <h1>COE</h1>
+          <NavLink to="/" className="header__logo">
+            <h1>COE</h1>
+          </NavLink>
         </div>
         <div className="header__middle">
           <h1>THE 16TH SET</h1>
